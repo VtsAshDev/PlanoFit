8f6b6224452ccc50d008219f4cc0fe0d7d5927e1
VitorVts <vitorgguimaraes56@gmail.com>
2024-02-05 14:09:09 -0300

    Efetuando testes de modal com o sweetalert2

--- src/js/apis.js
+++ src/js/apis.js
@@ -1,5 +1,5 @@
 //chat e form
-const apiKey = "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";
+const apiKey = "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";
 const submit = document.querySelector("#send");
 
 var userData = [];
@@ -54,32 +54,40 @@ function getTmb(sexo, idade, altura, kg, objetivo) {
       ? 655 + 9.6 * kg + 1.8 * altura - 4.7 * idade
       : 66 + 13.7 * kg + 5 * altura - 6.8 * idade
   );
-  console.log(tmb);
+
   return planoGpt(objetivo, sexo, tmb);
 }
 
 function planoGpt(objetivo, sexo, tmb) {
   let plano;
   if (objetivo === "emagrecimento") {
-    plano = `Plano alimentar para emagrecimento para ${sexo} de acordo com tmb ${tmb}kcal`;
+    plano = `Plano alimentar para emagrecimento informando as calorias das refeicoes para ${sexo} de acordo com tmb ${tmb}kcal`;
   } else if (objetivo === "saude") {
-    plano = `Plano alimentar saudável para ${sexo} de acordo com tmb ${tmb}kcal`;
+    plano = `Plano alimentar saudável informando as calorias das refeicoes para ${sexo} de acordo com tmb ${tmb}kcal`;
   } else if (objetivo === "muscular") {
-    plano = `Plano alimentar para ganho de massa muscular para ${sexo} de acordo com tmb ${tmb}kcal`;
+    plano = `Plano alimentar para ganho de massa muscular informando as calorias das refeicoes para ${sexo} de acordo com tmb ${tmb}kcal e junto a isso um treino de musculação`;
   }
   console.log(plano);
-  return sendMessage(plano)
+  return sendMessage(plano);
 }
 
 function sendMessage(plano) {
-  //   if (!userData.value) {
-  //     alert("Complete o Formulário");
-  //     return;
-  //   }
+  console.log(plano);
+  //
   var status = document.getElementById("status");
   var btnsubmit = document.getElementById("send");
   status.style.display = "block";
-  status.innerHTML = "Carregando...";
+  status.innerHTML = `
+    <div role="status">
+      <svg aria-hidden="true" class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-yellow-400" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+      </svg>
+      <span class="sr-only">Loading...</span>
+    </div>
+  
+`;
+
   btnsubmit.disabled = true;
   btnsubmit.style.cursor = "not-allowed";
 
@@ -91,7 +99,7 @@ function sendMessage(plano) {
       Authorization: `Bearer ${apiKey}`,
     },
     body: JSON.stringify({
-      model: "gpt-3.5-turbo",
+      model: "gpt-3.5-turbo-instruct",
       prompt: plano,
       max_tokens: 2048,
       temperature: 0.5,
@@ -99,6 +107,7 @@ function sendMessage(plano) {
   })
     .then((response) => response.json())
     .then((response) => {
+      console.log(response);
       chatReturn = response.choices[0].text;
       status.style.display = "none";
       chatResponseContent(chatReturn);
@@ -113,69 +122,63 @@ function sendMessage(plano) {
 }
 
 function chatResponseContent(response) {
-  var historic = document.getElementById("historic");
+  let formattedResponse = formatTextWithLineBreaks(response);
 
-  var boxchatResponseMessage = document.createElement("p");
-  boxchatResponseMessage.className = "box-response-message";
-  var chatResponse = document.createElement("p");
-  chatResponse.className = "chat-message";
-  chatResponse.innerHTML = response;
-
-  boxchatResponseMessage.appendChild(chatResponse);
-  historic.appendChild(boxchatResponseMessage);
+  return handleGetFormValues(formattedResponse);
 }
 
-// let send = document.querySelector('#send')
+function formatTextWithLineBreaks(text) {
+  let parts = text.split(/(?=[A-Z])/);
 
-// send.addEventListener('click',event =>{
-//     event.preventDefault();
-//     handleSubmitForm();
-// })
+  let formattedText = parts.join("\n");
 
-// function handleGetFormValues(){
-//     const whatsapp = document.getElementById('whatsapp').value
-//     const message = document .getElementById('message').value
+    
 
-//     if(typeof whatsapp !== 'string' || whatsapp ===""){
-//         return alert('Digite um número de Whatsapp válido')
-//     }
-//     if(typeof message !== 'string' || message === ""){
-//         return alert ("Digite uma mensagem")
-//     }
-//     return{
-//         whatsapp,
-//         message
-//     }
-// }
+  return formattedText;
+}
 
-// async function handleSubmitWhatsappMessage(phone,message){
-//     const GZAPPY_URL = "https://api.gzappy.com/v1/message/send-message"
+function handleGetFormValues(formattedResponse) {
+  const whatsapp = document.getElementById("wpp").value;
+  console.log(whatsapp);
+  const message = formattedResponse;
 
-// const response = await fetch(GZAPPY_URL, {
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json',
-//     "user_token_id": '648b6267-dfaf-4c82-9131-bc17444f34ed'
-//   },
-//   body: JSON.stringify({
-//     instance_id: "EJIUWMULK81JFQ0C7QAL9W5D",
-//     instance_token: "3f29d85e-bb36-4073-8b2a-3a3333b47c5e",
-//     message: [message],
-//     phone: phone
-//   })
-// })
+  if (typeof whatsapp !== "string" || whatsapp === "") {
+    return alert("Digite um número de Whatsapp válido");
+  }
+  if (typeof message !== "string" || message === "") {
+    return alert("Digite uma mensagem");
+  }
 
-// const data = await response.json()
+  return handleSubmitWhatsappMessage(whatsapp, message);
+}
 
-// console.log(data)
+async function handleSubmitWhatsappMessage(whatsapp, message) {
+  const GZAPPY_URL = "https://api.gzappy.com/v1/message/send-message";
 
-// }
+  const response = await fetch(GZAPPY_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      user_token_id: "648b6267-dfaf-4c82-9131-bc17444f34ed",
+    },
+    body: JSON.stringify({
+      instance_id: "EJIUWMULK81JFQ0C7QAL9W5D",
+      instance_token: "3f29d85e-bb36-4073-8b2a-3a3333b47c5e",
+      message: [message],
+      phone: whatsapp,
+    }),
+  });
 
-// async function handleSubmitForm(){
-//     const data = handleGetFormValues();
-//     console.log(data)
-//     if(data){
-//         await handleSubmitWhatsappMessage(data.whatsapp,data.message)
-//         console.log(data.whatsapp)
-//     }
-// }console.log('vtnc')
+  const data = await response.json();
+
+  console.log(data);
+}
+
+async function handleSubmitForm() {
+  const data = handleGetFormValues();
+  console.log(data);
+  if (data) {
+    await handleSubmitWhatsappMessage(data.whatsapp, data.message);
+    console.log(data.whatsapp);
+  }
+}
