2a87872e3ba06f93ae71c7bbc9ebfa52f1079661
VitorVts <vitorgguimaraes56@gmail.com>
2024-02-05 14:08:23 -0300

    Criado o vinculo com api para envio do plano via wpp

---
+++ main.js
@@ -0,0 +1,10 @@
+// let teste = document.getElementById('teste')
+
+
+// teste.addEventListener('click',(e)=>{
+//     Swal.fire({
+//         title: "Good job!",
+//         text: "You clicked the button!",
+//         icon: "success"
+//       });
+// })
