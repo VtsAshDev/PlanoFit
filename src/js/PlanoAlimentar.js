0e9f59cad576a21a5ac2a4fb14b4777d1d682897
VitorVts <vitorgguimaraes56@gmail.com>
2025-04-22 12:10:56 -0300

    fix: Format to pdf

--- src/js/PlanoAlimentar.js
+++ src/js/PlanoAlimentar.js
@@ -8,36 +8,24 @@ export class PlanoAlimentar {
   formatarPlano() {
     let formatted = this.resposta;
 
-    // Substituindo novas linhas para adicionar uma linha extra entre os parágrafos
-    formatted = formatted.replace(/\n/g, '<br><br>');
+    formatted = formatted.replace(/\*\*(.*?)\*\*/g, (match, p1) => `<b>${p1}</b>`);
 
-    // Convertendo negrito (**) para <b> e separando com quebras de linha
-    formatted = formatted.replace(/\*\*(.*?)\*\*/g, (match, p1) => {
-        return `<b>${p1}</b>`;
-    });
-
-    // Convertendo itálico (*) para <i>
-    formatted = formatted.replace(/\*(.*?)\*/g, (match, p1) => `<i>${p1}</i>`);
-
-    // Convertendo cabeçalhos (##) para <h2>
     formatted = formatted.replace(/## (.*?)\n/g, (match, p1) => `<h2>${p1}</h2>`);
 
-    // Convertendo lista (com *) para <ul> e <li>
     formatted = formatted.replace(/\* (.*?)\n/g, (match, p1) => `<ul><li>${p1}</li></ul>`);
+
 
     return formatted;
   }
-  
+
   gerarPDF() {
     const doc = new jsPDF();
 
     const formattedContent = this.formatarPlano();
 
-    // Criando um contêiner temporário para inserir o HTML
     const tempElement = document.createElement('div');
     tempElement.innerHTML = formattedContent;
 
-    // Usando jsPDF para gerar o PDF a partir do HTML formatado
     doc.html(tempElement, {
       margin: [10, 10, 10, 10],
       x: 10,
@@ -47,10 +35,12 @@ export class PlanoAlimentar {
       autoPaging: true,
       html2canvas: {
         scale: 0.2
+      },
+      callback: function (doc) {
+        doc.save('plano-alimentar.pdf');
       }
     });
-
-    doc.save('plano-alimentar.pdf');
   }
 }
+
 
